696a4f60bb2f118a3905c7507769862de109f43f
Vasiliy Asmuz <asmuz@mail.ru>
2022-03-25 18:24:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,24 +8,21 @@
 </head>
 <body>
 	<div class="section full-height">
-		<input class="modal-btn" type="checkbox" id="modal-btn" name="modal-btn" />
-		<label for="modal-btn">Кликни</label>
-		<div class="modal">
-			<div class="modal-wrap">
+
+      	<input class="modal-btn" type="checkbox" id="modal-btn" name="modal-btn"/>
+      	<label for="modal-btn">Кликни</label> 		
+      	
+		<div class="modal">		
+	      	<div class="modal-wrap">	
 				<form id="form" action="/amo/amo.php" method="post" onsubmit="javascript:return emailValidate();">
-					<div class="row">
-						<label for="phone">Введите телефон для подтверждения доступа:</label>
-					</div>
-					<div class="row">
-						<input id="phone" name="phone" placeholder="+7 (000) 000-00-00" data-phone-pattern required /> </div>
-					<div class="row">
-						<label for="email">Введите Email для получения файлов:</label>
-					</div>
-					<div class="row">
-						<input type="text" name="email" id="email" placeholder="E-mail" required /> </div>
-					<input type="submit" value="Скачать файлы"> </form>
-			</div>
-		</div>
+					<label for="phone">Введите телефон для подтверждения доступа:</label>
+					<input id="phone" name="phone" placeholder="+7 (000) 000-00-00" data-phone-pattern required />
+					<label for="email">Введите Email для получения файлов:</label>
+					<input type="text" name="email" id="email" placeholder="E-mail" required />
+					<input type="submit" value="Скачать файлы">
+				</form>
+	      	</div>			          		
+      	</div>	
 	</div>
 </body>
 </html>
